99ae1383d6011e752ab4f886a49a9d8bf94e9363
DmitrySobol1 <saibleonetwo@gmail.com>
2025-03-16 17:21:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import axios from './axios';
+import { useState } from 'react';
+
+const App = () => {
+
+    const [score,setScore] = useState(1105)
+
+    function clickHandler() {
+        console.log('clicked')
+        setScore(score+1)
+        
+        axios.post('/scoreincrement', {
+            tlgid: 20,
+          })
+          .then(response => {
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.error('Ошибка:', error);
+          });
+    
+}
+
+  return (
+    <>
+      <div>{score}</div>
+      
+      <Button variant="contained" color="primary" onClick={clickHandler}>
+        Нажми меня
+      </Button>
+    </>
+  );
+};
+
+export default App;
