b9afac931c4902ca81852d592311dd002c190981
DmitrySobol1 <saibleonetwo@gmail.com>
2025-03-28 01:13:45 +0300

    27-03 cnange09

--- src/Components/BottomModal/BottomModal.jsx
+++ src/Components/BottomModal/BottomModal.jsx
@@ -1,46 +1,53 @@
 import React from 'react';
-import style from './BottomModal.module.css'
+import style from './BottomModal.module.css';
 
 import { LanguageContext } from '../../App';
 import { BottomModalContext } from '../../App';
 import { useContext, useEffect } from 'react';
-import {TEXTS} from './texts'
-
+import { TEXTS } from './texts';
 
 const BottomModal = (props) => {
+  const { isShowBottomModal, setShowBottomModal } =
+    useContext(BottomModalContext);
+  const { language, setLanguage } = useContext(LanguageContext);
 
-const { isShowBottomModal, setShowBottomModal } = useContext(BottomModalContext);
-const { language, setLanguage } = useContext(LanguageContext);
- 
- const { title, text } = TEXTS[props.props]?.[language];
-    
- function closeBtnHandler() {
-    setShowBottomModal(!isShowBottomModal)
- }
+  const { title, text } = TEXTS[props.props]?.[language];
 
-console.log(props)
+  function closeBtnHandler() {
+    setShowBottomModal(!isShowBottomModal);
+  }
 
+  console.log(props);
 
-useEffect(() => {
+  useEffect(() => {
     console.log('Компонент смонтирован для', props);
-    
+
     // return () => {
     //   console.log('Компонент будет размонтирован');
     // };
   }, []); // Пустой массив = нет зависимостей
 
-
-
-
+  // return (
+  //   <div className={style.modal}>
+  //     <button onClick={closeBtnHandler}>
+  //       <span className={style.closeBtn}>&times;</span>
+  //     </button>
+  //     <div className={style.title}>{title}</div>
+  //     <div>{text}</div>
+  //   </div>
+  // );
 
   return (
-    
-    <div className={style.modal}>
-      
-        <button onClick={closeBtnHandler}><span className={style.closeBtn}>&times;</span></button>
-        <div className = {style.title}>{title}</div>
-        <div>{text}</div>
-      
+    <div
+      className={`${style.modal} ${
+        isShowBottomModal ? style.modalVisible : ''
+      }`}
+    >
+      <button onClick={closeBtnHandler}>
+        <span className={style.closeBtn}>&times;</span>
+      </button>
+      <div className={style.title}>{title}</div>
+      <div>{text}</div>
     </div>
   );
 };
